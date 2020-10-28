4946260606219a90dda6611c18e30885ec734b5f
monkey123123 <you@example.com>
2020-10-28 18:38:04 +0800

    完善powerjob文档

--- concurrent-system/src/test/java/me/monkey/IntegerTest.java
+++ concurrent-system/src/test/java/me/monkey/IntegerTest.java
@@ -1,5 +1,9 @@
 package me.monkey;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.pattern.Patterns;
+import akka.util.Timeout;
 import com.alibaba.fastjson.parser.ParserConfig;
 import me.monkey.util.YamlData;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -11,13 +15,37 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import scala.concurrent.Future;
+import scala.concurrent.duration.Duration;
 
 import java.io.IOException;
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class IntegerTest {
+/*
+
+    @Test
+    public void startTest3() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        Timeout t = new Timeout(Duration.create(1, TimeUnit.SECONDS));
+        ActorSystem system = ActorSystem.create();
+        ActorRef myActor = system.actorOf(ActorTimeOut.props());
+        //使用ask发送消息,actor处理完，必须有返回（超时时间5秒）
+        Future<Object> ask = Patterns.ask(myActor, "Hello,world", t);
+        ask.onComplete(new OnComplete<Object>() {
+            public void onComplete(Throwable throwable, Object o) throws Throwable {
+                if (throwable != null) {
+                    System.out.println("some thing wrong." + throwable);
+                } else {
+                    System.out.println("success:" + o);
+                }
+            }
+        }, system.dispatcher());
+    }
+*/
 
     int count = 10000;
     CountDownLatch latch = new CountDownLatch(1);
