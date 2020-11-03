fd2f74f41a9b2c14b1e51de5303f79a316f5a635
monkey123123 <you@example.com>
2020-11-03 17:36:55 +0800

    去除自定义的mongodb事务管理器

--- concurrent-system/src/main/java/me/monkey/config/TransactionConfig.java
+++ concurrent-system/src/main/java/me/monkey/config/TransactionConfig.java
@@ -5,7 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 
-@Configuration
+/*
+暂时不用，先注掉
+ */
+//@Configuration
 public class TransactionConfig {
     @Bean
     MongoTransactionManager transactionManager(MongoDbFactory factory){
