6724ce7dc5a9c23dbf9b3707b18cdf5ec4bc727d
monkey123123 <514081681@qq.com>
2020-12-03 18:49:40 +0800

    zipkin链路追踪

---
+++ concurrent-zipkin/src/main/java/me/monkey/zipkin/ZipkinApplication.java
@@ -0,0 +1,16 @@
+package me.monkey.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin.server.internal.EnableZipkinServer;
+
+//@EnableDiscoveryClient
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinApplication.class, args);
+	}
+}
