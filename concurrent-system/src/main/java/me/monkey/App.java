2747ede674d5f7aac4c41c8d961fed2ebdf0dc1b
monkey123 <514081681@qq.com>
2022-04-13 19:48:58 +0800

    切换注册中心至nacos集群

--- concurrent-system/src/main/java/me/monkey/App.java
+++ concurrent-system/src/main/java/me/monkey/App.java
@@ -5,6 +5,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
@@ -12,8 +14,8 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  */
 //@Configuration
 //@EnableAutoConfiguration
-//@EnableDiscoveryClient
-//@EnableFeignClients
+@EnableDiscoveryClient
+@EnableFeignClients
 //@EnableCircuitBreaker
 //@EnableHystrix//add by allen
 
